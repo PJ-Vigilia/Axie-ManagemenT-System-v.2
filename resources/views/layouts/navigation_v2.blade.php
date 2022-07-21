bb15b24ace1450fcf0adec02e02ec42ff5bc0cb4
PJ-Vigilia <pjvigilia21@gmail.com>
2022-07-21 13:46:40 +0900

    /

--- resources/views/layouts/navigation_v2.blade.php
+++ resources/views/layouts/navigation_v2.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-white  navigation">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">AIMS</a>
+        <a class="navbar-brand" href="#">A.I.M.S.</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
